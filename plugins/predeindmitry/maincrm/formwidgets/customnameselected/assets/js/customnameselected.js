3ebf339e676fda3e084e41074f87eaaaca1ea721
DmitryPredein <predein.d5@gmail.com>
2022-03-14 11:52:28 +0500

    Fix Порог поиск по фио в 3 символа

--- plugins/predeindmitry/maincrm/formwidgets/customnameselected/assets/js/customnameselected.js
+++ plugins/predeindmitry/maincrm/formwidgets/customnameselected/assets/js/customnameselected.js
@@ -2,16 +2,31 @@ $(document).ready(function() {
 	let nameSelected = {
 		init: function (){
 			let _this = nameSelected;
-			
+
 			$(document).on("focus", ".LayautsFlexColumn .form-controlOtchest", function(){
-				_this.show(this);
-				_this.selectedFilter(this);
+				if ($(this).val().length >= 3) {
+					_this.show(this);
+					_this.selectedFilter(this);
+				}
 			});
-			
+
+			$(document).on("input", ".LayautsFlexColumn .form-controlOtchest", function(){
+				if ($(this).val().length >= 3) {
+					_this.show(this);
+					_this.selectedFilter(this);
+				}
+			});
+
+			$(document).on("input", ".LayautsFlexColumn .form-controlOtchest", function(){
+				if ($(this).val().length < 3) {
+					_this.hide(this);
+				}
+			});
+
 			$(document).on("blur", ".LayautsFlexColumn .form-controlOtchest", function(){
 				_this.hide(this);
 			});
-			
+
 			$(document).on("click",  ".LayautsFlexColumn .formNameSelectedUl li", function(){
 				let valName = $(this).data("name");
 				let valTel = $(this).data("tel");
@@ -19,7 +34,7 @@ $(document).ready(function() {
 				_this.selected(valName, valTel, valId);
 				$("#controlOtchestIdTel").attr("disabled", true);
 			});
-			
+
 			$(document).on("input",  ".LayautsFlexColumn .form-controlOtchest, #controlOtchestIdTel", function(){
 				if($("#controlOtchestFinal").data("check") == "select"){
 					_this.selectedDel(this);
@@ -28,7 +43,7 @@ $(document).ready(function() {
 				_this.selectedFilter(this);
 				_this.addInputFinal(id = null);
 			});
-			
+
 			$(document).on("input",  "#controlOtchestIdTelUpdate, #controlOtchestIdNameUpdate", function(){
 				if($(this).val() != ""){
 					_this.updateName();
@@ -36,7 +51,7 @@ $(document).ready(function() {
 				else{
 					_this.selectedDel(elem = "update");
 				}
-				
+
 			});
 		},
 		show: function(elem){
@@ -48,16 +63,16 @@ $(document).ready(function() {
 		selected: function(valName, valTel, valId){
 			let _this = nameSelected;
 			valName = valName.split(' ');
-			
+
 			let firstName = valName[0];
 			let name = valName[1];
 			let lastName = valName[2];
-			
+
 			$("#controlOtchestIdTel").val(valTel);
 			$("#controlOtchestIdFirstName").val(firstName);
 			$("#controlOtchestIdName").val(name);
 			$("#controlOtchestIdLastName").val(lastName);
-			
+
 			$("#controlOtchestFinal").data("check", "select");
 			_this.addInputFinal(valId);
 		},
@@ -105,7 +120,7 @@ $(document).ready(function() {
 			}
 			else{
 				$("#controlOtchestFinal").val($("#controlOtchestFinal").data("check")+":::"+id);
-			}	
+			}
 		},
 		updateName: function(){
 			$("#controlOtchestFinalUpdate").val("update"+":::"+$("#controlOtchestFinalUpdate").data("id")+":::"+$("#controlOtchestIdNameUpdate").val()+":::"+$("#controlOtchestIdTelUpdate").val());
@@ -120,7 +135,7 @@ $(document).ready(function() {
 			});
 		},
 	};
-	
+
 	nameSelected.init();
 	maskTel.init();
 });
